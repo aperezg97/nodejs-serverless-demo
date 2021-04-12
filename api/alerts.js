e876588626212c9b182c84fc02f09e0875fccdad
Alex Perez <alex.perez@union.co>
2021-04-12 13:37:49 -0600

    Dummy endpoints, sending hardcoded email, loading dotenv file.

---
+++ api/alerts.js
@@ -0,0 +1,35 @@
+const emailService = require('../services/email.service');
+
+const sendEmailNotification = async (event, context, callback) => {
+    let responseData;
+    try {
+        const result = await emailService.sendEmail();
+        responseData = { statusCode: 200, body: JSON.stringify(result) };
+    } catch (ex) {
+        console.log(ex);
+        responseData = { statusCode: 500, body: JSON.stringify(ex, Object.getOwnPropertyNames(ex)) };
+    }
+    callback(null, responseData);
+}
+
+const verifyEmailSMTPService = async (event, context, callback) => { 
+    let responseData;
+    try {
+        const result = await emailService.verifyEmailSMTPService();
+        responseData = { statusCode: 200, body: JSON.stringify(result) };
+    } catch (ex) {
+        responseData = { statusCode: 500, body: JSON.stringify(ex, Object.getOwnPropertyNames(ex)) };
+    }
+    callback(null, responseData);
+}
+
+const envValues = (event, context, callback) => {
+    const responseData = { statusCode: 200, body: JSON.stringify({
+        APP_SMTP_SERVER: process.env.APP_SMTP_SERVER,
+        APP_SMTP_PORT: process.env.APP_SMTP_PORT,
+        env: process.env}) 
+    };
+    callback(null, responseData);
+}
+
+module.exports = {verifyEmailSMTPService, sendEmailNotification, envValues}
